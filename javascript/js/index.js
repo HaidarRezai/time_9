c5612c8ffcaf114422b2f06d1e139cf92df98d85
Haidar Rezai <haidarrezai4545@gmail.com>
2025-06-04 21:53:24 +0430

    obj

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,5 +1,25 @@
-function test(a, b, c, d, e, f) {
-
-    return (js + html + css + math) / 4
+const students = {
+    fullName: "Omid Ahamdi",
+    gender: "Male",
+    subject: ["HTML", "CSS", "JS"],
+    time: "9-10 pm",
+    course: function () {
+        return `${this.fullName}, ${this.gender}`
+    }
 }
-document.getElementById("num1").innerHTML = score(90, 100, 90, 90)
+
+document.getElementById("para").innerHTML = students.course()
+
+function getData() {
+    let full = document.getElementById("full").value;
+    let gender = document.getElementById("gender").value;
+    let time = document.getElementById("time").value;
+
+    students.fullName = full
+    students.gender = gender
+    students.time = time
+    alert(time)
+    document.getElementById("para").innerHTML = students.fullName
+    document.getElementById("para").innerHTML += students.gender
+    document.getElementById("para").innerHTML += students.time
+}
