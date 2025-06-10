b2cd4a567ea3a88223eec2f3108daf56e86fd0ca
Ahmad Sulaimankhail <ahmadsulaimankhail034@gmail.com>
2025-06-10 18:07:09 +0200

    upts

---
+++ home work/Nazanin/home 2js/home 2js.js
@@ -0,0 +1,9 @@
+    function oky() {
+     let x;
+     let y;
+
+
+     x =document.getElementById("username").value;
+     y =document.getElementById("password").value;
+     document.getElementById("demo").innerHTML=x+ " "+y;
+    }
