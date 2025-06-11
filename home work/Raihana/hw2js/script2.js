942167c1181b9712a9d802b36ef60a9d0857bdba
Sulaimankhail <samimsulaimankhail02@gmail.com>
2025-06-11 17:11:29 +0200

    updet

--- home work/Raihana/hw2js/script2.js
+++ home work/Raihana/hw2js/script2.js
@@ -1,7 +1,17 @@
-function info(){
-    let x;
-    let y;
-    x = document.getElementById("username").value;
-    y = document.getElementById("password").value;
-    document.getElementById("demo").innerHTML = x + "  " + y;
+const info = {
+    username: "raihana",
+    password: "123456"
+}
+
+function login(){
+   let x = document.getElementById("username").value;
+   let y = document.getElementById("password").value;
+
+   if(x === info.username && y === info.password){
+    window.alert("Welcome in our Website." + x + y)
+   }
+   else{
+    alert("worng password")
+   }
+
 }
