843e1e13585ea860cf748042ffda9ce5f8828948
Haidar Rezai <haidarrezai4545@gmail.com>
2025-06-11 22:09:33 +0430

    uses in info data

--- home work/Tamim/JS HW1/login.js
+++ home work/Tamim/JS HW1/login.js
@@ -1,17 +1,37 @@
-const info = {
-   Username:"admin",
-   Password:"admin"
-}
+const info = [
+    {
+        full_name: "Samim Sulimani",
+        Username: "admin1",
+        Password: "admin"
+    },
+    {
+        full_name: "Ahmad Sulimani",
+        Username: "admin2",
+        Password: "admin"
+    },
+    {
+        full_name: "Nazanin Sulimani",
+        Username: "admin3",
+        Password: "admin"
+    },
+]
+/*
 
-function getdata(){
-   let x = document.getElementById('username').value;
-   let y = document.getElementById('password').value;
-    info.Username =x;
-    info.Password =y;
-    if(x === info.Username && y === info.Password){
-        alert('Your username ' + x + ' and Your Password '+ y +' are correct');
+1 loop 
+    1 if
+    2 break;
+
+
+*/
+function getdata() {
+    let x = document.getElementById('username').value;
+    let y = document.getElementById('password').value;
+    info.Username = x;
+    info.Password = y;
+    if (x === info.Username && y === info.Password) {
+        alert('Your username ' + x + ' and Your Password ' + y + ' are correct');
     }
-    else{
-        alert('Your Username '+ x +'and Password ' + y +'is wrong');
+    else {
+        alert('Your Username ' + x + 'and Password ' + y + 'is wrong');
     }
 }
