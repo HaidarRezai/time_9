5d0b4a54b67b7e5d3fc5cbc1610e416dff410128
Ahmad Sulaimankhail <ahmadsulaimankhail034@gmail.com>
2025-06-11 17:33:15 +0200

    up

--- home work/Nazanin/home 2js/home 2js.js
+++ home work/Nazanin/home 2js/home 2js.js
@@ -1,9 +1,25 @@
-    function oky() {
-     let x;
-     let y;
+      const info ={
+      username: "nazanin" , 
+      password:"2222"
 
+    } 
+
+     
+
+
+     function oky() {
+     let x;
+     let y; 
 
      x =document.getElementById("username").value;
      y =document.getElementById("password").value;
-     document.getElementById("demo").innerHTML=x+ " "+y;
+    //  document.getElementById("demo").innerHTML=x+ " "+y;
+
+     if ( x === info.username && y === info.password){
+      alert("your username"+ x + " " + " and your password"+ y +" are correct");
+     }
+    
+    else {
+      alert("worng username and password")
     }
+  }
