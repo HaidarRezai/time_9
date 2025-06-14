c1c3a30d6bc23f015f7388dfb960631a6f9eac5c
Tamimskl <tamimskl697@gmail.com>
2025-06-14 20:56:59 +0430

    g

--- home work/Tamim/JS HW1/login.js
+++ home work/Tamim/JS HW1/login.js
@@ -1,17 +1,32 @@
-const info = {
-   Username:"admin",
-   Password:"admin"
-}
+const info = [
+    { Username: "admin", Password: "admin" },
+    { Username: "tamim", Password: "tamim" },
+    { Username: "khan", Password: "khan" }
+];
 
-function getdata(){
-   let x = document.getElementById('username').value;
-   let y = document.getElementById('password').value;
-    info.Username =x;
-    info.Password =y;
-    if(x === info.Username && y === info.Password){
-        alert('Your username ' + x + ' and Your Password '+ y +' are correct');
+function getdata() {
+    let x = document.getElementById('username').value;
+    let y = document.getElementById('password').value;
+    // info.Username =x;
+    // info.Password =y;
+    // if(x === info.Username && y === info.Password){
+    //     alert('Your username ' + x + ' and Your Password '+ y +' are correct');
+    // }
+
+    // else{
+    //     alert('Your Username '+ x +' and Password ' + y +' is wrong');
+    // }
+
+    for (let i = 0; i < info.length; i++) {
+        if (info[i].Username === x && info[i].Password === y) {
+            alert("Your username " + info[i].Username + " and password is "+ info[i].Password + "       correct");
+            return;
+        }
+
     }
-    else{
-        alert('Your Username '+ x +'and Password ' + y +'is wrong');
-    }
-}
+    alert("Username is not exist in the Database");
+};
+
+
+
+
