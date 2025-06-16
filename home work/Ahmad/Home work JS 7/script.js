8866d6a1ed210d3e60030a16c89401416251e148
Ahmadsulaimankhail <ahmadsulaimankhail034@gmail.com>
2025-06-16 16:22:55 +0200

    7hwa

---
+++ home work/Ahmad/Home work JS 7/script.js
@@ -0,0 +1,14 @@
+function darkMode() {
+    document.getElementById('body').classList.toggle("dark_mode")
+}
+function addToDisplay(value) {
+    document.getElementById("display").value += value + "";
+}
+function remove() {
+    let x = document.getElementById("display");
+    x.value = x.value.slice(0,-1);
+}
+function removeAc() {
+    let x = document.getElementById("display");
+    x.value = x.value.slice(-1,0);
+}
