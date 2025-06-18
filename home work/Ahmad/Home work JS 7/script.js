34917b0b82eb605939b5f8786de7d2f5e96db784
Ahmadsulaimankhail <ahmadsulaimankhail034@gmail.com>
2025-06-18 18:08:54 +0200

    update7,7

--- home work/Ahmad/Home work JS 7/script.js
+++ home work/Ahmad/Home work JS 7/script.js
@@ -12,3 +12,13 @@ function removeAc() {
     let x = document.getElementById("display");
     x.value = x.value.slice(-1,0);
 }
+
+function sum(){
+    let plus = document.getElementById('display').value;
+    plus = plus.split('+')
+    let result = 0
+    for(i of plus){
+        result += Number(i)
+    }
+    document.getElementById('display').value = result;
+}
