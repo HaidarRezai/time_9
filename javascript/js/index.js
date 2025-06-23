06baf569ff4d3cac1165082108d4e63f12b1d679
Haidar Rezai <haidarrezai4545@gmail.com>
2025-06-23 21:41:01 +0430

    lesson

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,17 +1,28 @@
-const arr = [
-    1, 2, 3, 4, 5
-]
-const names = [
-    "Ahmad", "Samim", "Tamin", "Rahana", "Nazanin"
-]
+/*
 
-const result = new Array()
-function test() {
-    arr.sort(function () { return 0.5 - Math.random() });
-    for (i of arr) {
-        result.push(names[i - 1])
-    }
-    document.getElementById("page").innerHTML = `${result}`
+document.getElementById("names").innerHTML += "Omid"
+document.getElementById("names").innerHTML += "Ahamd"
+document.getElementById("names").innerHTML += "Farid"
+document.getElementById("names").innerHTML += "Abbass"
+document.getElementById("names").innerHTML += "Hamid"
 
+loop 
+    1 while
+    2 do while
+    3 for 
+    4 for in 
+    5 for of 
+    6 map
+    7 forench
+
+
+for(let i = 1;i<=10;i++){
+    stement1
+    stement2
+    stement3
 }
-
+*/
+let arr = "google clude center"
+for (let i = arr.length - 1; i >= 0; i--) {
+    document.getElementById("names").innerHTML += arr[i]
+}
